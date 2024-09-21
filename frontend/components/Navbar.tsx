8dc9f5ced9fddc193da5ad85031f7720ec0cbbe4
bassimeledath <bassimfaizal@gmail.com>
2024-09-21 16:01:26 -0700

    chore: Add logo to navbar

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Navbar: React.FC = () => {
     return (
-        <nav className="bg-[#dac5fe] p-4">
-            <div className="text-2xl font-bold text-[#fb882f]">OptoPrompt</div>
+        <nav className="bg-[#dac5fe] p-4 pl-6">
+            <div className="flex items-center">
+                <Image
+                    src="/logo.png"
+                    width={225}
+                    height={225}
+                    alt="Logo"
+                />
+            </div>
         </nav>
     )
 }
